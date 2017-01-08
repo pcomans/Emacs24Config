ca998295ff102497d6b01b278c63f56c407c7793
Philipp Comans <philipp.comans@gmail.com>
2017-01-08 10:56:07 -0800

    Replace Marmalade with MELPA

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;Emacs Starter Kit
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
